08bac09b5cf62e01bf996b89e00b41e86ad6e093
ZUXXSU <hardik.kolge.it@gmail.com>
2025-10-18 13:15:09 +0530

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,21 +6,22 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    // id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 android {
-    namespace = "com.example.login"
+    namespace = "com.abcd.login"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        // This enables the feature required by flutter_local_notifications
+        coreLibraryDesugaringEnabled = true 
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
@@ -28,10 +29,11 @@ android {
         applicationId = "com.example.login"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -47,8 +49,10 @@ flutter {
     source = "../.."
 }
 dependencies {
-  implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+  implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
 
   implementation("com.google.firebase:firebase-analytics")
-
+  
+  // Add this line for core library desugaring
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
